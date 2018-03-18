b77ad44835435bf84b4068067faf8ab775029af6
zhangjianjun <403549730@qq.com>
2018-03-18 17:14:17 +0800

    feat: 钩子和修改星级

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.scss']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit,OnChanges {
+
   @Input()
   rating: number = 0;
   stars: boolean[];
-
+  @Input()
+  readonly: boolean = true;
+  @Output()
+  ratingChange:EventEmitter<number>=new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  ngOnChanges(changes: SimpleChanges): void {
     this.stars = [];
     for (let i = 1; i <= 5; i++) {
       this.stars.push(i > this.rating);
     }
-
+  }
+  
+  clickStar(index: number) {
+    if(!this.readonly){
+      this.rating = index + 1;
+      this.ratingChange.emit(this.rating);
+    }
+   
   }
 
 }
